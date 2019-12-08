30c2614181422982ccce5f91149be822c6d66c6d
shanzm <2351144690@qq.com>
2019-12-09 03:18:05 +0800

    2019年12月9日 03:17:57-update

--- 001EFDemo/TestDbContext.cs
+++ 001EFDemo/TestDbContext.cs
@@ -7,11 +7,14 @@ using System.Threading.Tasks;
 
 namespace _001EFDemo
 {
-    class TestDbContext:DbContext
+    class TestDbContext : DbContext
     {
         public TestDbContext() : base("name=connStr")
         {
             //name=connSt 表示使用连接字符串中名字为 connStr 的去连接数据库
+
+            //禁止EF自动根据我们定义的数据类建表
+            Database.SetInitializer<TestDbContext>(null);
         }
         public DbSet<Person> Persons { get; set; }//泛型集合类型
     }
