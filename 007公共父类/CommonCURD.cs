d9bfbf6b1fca131bd44abda90eefab6a1e21ffad
shanzm <2351144690@qq.com>
2019-12-22 02:17:46 +0800

    2019年12月22日 02:17:41-update

--- 007公共父类/CommonCURD.cs
+++ 007公共父类/CommonCURD.cs
@@ -6,16 +6,13 @@ using System.Threading.Tasks;
 
 namespace _007公共父类
 {
-    class CommonCURD<T> where T : EntityBase//泛型类，指定泛型继续于BaseEntity类，换言之我们的这个类的操作仅针对BaseEntity类的子类对象
+    class CommonCURD<T> where T : EntityBase
+        //泛型类，使用where约束泛型，指定泛型继续于BaseEntity类，换言之我们的这个类的操作仅针对BaseEntity类的子类对象
     {
-        private MyDbContext cxt;
+        private MyDbContext cxt;//注意我们不是在下面的那些增删改查函数中new MyDBContext（）对象，而是在这个类外new MyDBContext（）再通过构造函数传入类中，防止数据库连接失效
         public CommonCURD(MyDbContext cxt)
         {
             this.cxt = cxt;
-        }
-
-        public CommonCURD()
-        {
         }
 
         /// <summary>
@@ -66,6 +63,12 @@ namespace _007公共父类
         {
             return cxt.Set<T>().OrderBy(e => e.CreateDateTime).Skip(start).Take(count).Where(e => e.IsDeleted == false);
         }
+        ///注意这里的返回值类型是IQueryable<T>类型，他继承与IEnumerable<T>类型，但是我们不要使用IEnumberable类型
+        ///为什么呢，假如我们的查询的数据的类中有ICollection<T>类型的属性，我们需要使用Include（）函数避免延迟加载
+        ///注意我们这里的查询结果是IQueryable<T>类型的对象，他的Include（）函数的命名空间是：System.Data.Entity
+
+
+
 
         /// <summary>
         /// 查询数据库的数据总量
