51f05250de99a067c670c56a2dec9365459b88aa
shanzm <2351144690@qq.com>
2019-12-23 13:26:05 +0800

    2019年12月23日 13:25:39-update 添加AddNew( )

--- 009SchoolWeb/Controllers/StudentController.cs
+++ 009SchoolWeb/Controllers/StudentController.cs
@@ -18,5 +18,42 @@ namespace SchoolWeb.Controllers
 
             return View(students);
         }
+
+        [HttpGet]
+        public ActionResult AddNew()
+        {
+            NationService ntnService = new NationService();
+            IEnumerable<SchoolDTO.NationDTO> nations = ntnService.GetAll();
+
+            //将民族包装为SelectList类型，传到View，提供给Html.DropDownList
+            //SelectList slNation = new SelectList(nations, "Id", "Name");
+            //使用nameof()函数获取变量名，可以避免打字错误，编译其更容易发现错误
+            //第二个参数：下拉列表的value属性,第三个参数：下拉列表的显示属性
+            SelectList slNation = new SelectList(nations, nameof(Nation.Id), nameof(Nation.Name));
+
+
+            //因为传输的数据比较单一，所以不封装为ViewModel类了
+            //todo:班级的DropDownlist
+            return View(slNation);
+        }
+
+        [HttpPost]
+        public ActionResult AddNew(string name, int age, long nationId)
+        {
+            StudentService stuService = new StudentService();
+            stuService.Add(name, age, nationId, 1);
+            // 重定向
+            // return RedirectToAction("Index");使用下面的nameof()，减少自己编码中出现字符串作为参数，可避免不必要的错误
+            //也可这么写：return Redirect("/Student/Index");
+            return RedirectToAction(nameof(StudentController.Index));
+        }
+
+        public ActionResult Delete(long id)
+        {
+            StudentService stuService = new StudentService();
+            stuService.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
